b9fc5e8604299fb368cfae29e3aeae72215c429d
vitor_medeiros <vitordaniel602@gmail.com>
2023-04-27 15:59:23 -0300

    Corrigindo alguns erros #1

--- tarefas/t01/q14.sql
+++ tarefas/t01/q14.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE VIEW total AS
+CREATE OR REPLACE VIEW totalatv AS
 SELECT count(*) as total, cod_responsavel
 FROM atividade
 group by cod_responsavel;
   
 select f.nome, t.total as total_atividades
-from funcionario f, total t
+from funcionario f, totalatv t
 where f.codigo = t.cod_responsavel
  
