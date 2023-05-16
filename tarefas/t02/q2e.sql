888c2a822bdef2e6c3665e39d343c3d41d5d0e24
vitor_medeiros <vitordaniel602@gmail.com>
2023-05-15 21:16:24 -0300

    Função para calcular quantos dias de atraso tem um projeto #2

--- tarefas/t02/q2e.sql
+++ tarefas/t02/q2e.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION calcular_atraso_atividade(data_prevista DATE, data_conclusao DATE)
+CREATE OR REPLACE FUNCTION calcular_atraso_projeto(data_prevista DATE, data_conclusao DATE)
 RETURNS INTEGER
 LANGUAGE plpgsql
 AS $$
@@ -6,7 +6,7 @@ DECLARE
   dias_atraso INTEGER;
 begin
   if data_conclusao is null then
-  	dias_atraso =  data_prevista - CURRENT_DATE;
+  	dias_atraso = data_prevista - CURRENT_DATE;
   else 
   	dias_atraso = data_prevista - data_conclusao;
   END IF;
